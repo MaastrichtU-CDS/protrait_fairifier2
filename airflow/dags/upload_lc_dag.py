9ea11c6a291bb558dddc1891d2ce6aba2efe3ed5
Aiara Lobo Gomes <aiara.lobogomes@maastro.nl>
2022-04-13 15:35:26 +0200

    Fixed ignoring of NaN values; adding gender back

--- airflow/dags/upload_lc_dag.py
+++ airflow/dags/upload_lc_dag.py
@@ -172,8 +172,8 @@ def upload_to_lc(
 
         items = ''
         for name in df.columns:
-            if name != identifier_colname and name != gender_colname:
-                if row[name] is not None:
+            if name != identifier_colname:
+                if not pd.isna(row[name]):
                     items += f'<ItemData ItemOID="{item_prefix}{name}" Value="{row[name]}"/>\n'
 
         subject = f'''
