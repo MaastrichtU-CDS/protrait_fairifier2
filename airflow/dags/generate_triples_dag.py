67dd3ce6bb562c5032e78364f13aa050fb5d3fd5
Aiara Lobo Gomes <aiara.lobogomes@maastro.nl>
2023-02-27 09:56:06 +0100

    Do not add subclasses

--- airflow/dags/generate_triples_dag.py
+++ airflow/dags/generate_triples_dag.py
@@ -63,6 +63,7 @@ with DAG(
 
     ontologies = {
         'roo': 'https://raw.githubusercontent.com/MaastrichtU-CDS/protrait_fairifier2/ontologies/ontologies/roo_new.owl',
+        'ncit': 'https://raw.githubusercontent.com/MaastrichtU-CDS/protrait_fairifier2/main/ontologies/subncit.owl',
         'protrait': 'https://raw.githubusercontent.com/MaastrichtU-CDS/protrait_term-mapper/main/proTRAITTermmapperValueTriples.ttl',
     }
 
